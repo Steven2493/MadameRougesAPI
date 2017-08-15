090c1230b7276f603a977c9bd07776abc28614dc
Steven Yuen <email": null>
2017-08-15 12:06:24 -0400

    json for new user and new sessions

--- madamerouge/app/controllers/users_controller.rb
+++ madamerouge/app/controllers/users_controller.rb
@@ -5,21 +5,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    p params[:user]
     @user = User.new(user_params)
 
     if @user.save
       session[:user_id] = @user.id
-      redirect_to "/"
+      render json: {id: @user.id}
     else
-      render 'new'
+      render :status => 503
     end
 
   end
 
-  # private
+  private
 
-  # def user_params
-  #   params.require(:user).permit(:first_name, :last_name, :email, :password)
-  # end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
 end
