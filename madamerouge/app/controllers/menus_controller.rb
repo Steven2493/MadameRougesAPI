12302d90817349fe3f3cbd69c498792badfab960
Steven Aguilar <stevenaguilar24@gmail.com>
2017-08-15 18:10:25 -0400

    styling and some db changes

--- madamerouge/app/controllers/menus_controller.rb
+++ madamerouge/app/controllers/menus_controller.rb
@@ -1,11 +1,14 @@
 class MenusController < ApplicationController
   def index
     @menu = Menu.current
+    # @menu.start_at = @menu.start_at.strftime("%m/%d/%Y")
+    # @menu.end_at = @menu.end_at.strftime("%m/%d/%Y")
+    @menu_hash = {end_at: @menu.end_at.strftime("%m/%d/%Y"), start_at: @menu.start_at.strftime("%m/%d/%Y"), id: @menu.id}
     @info = []
     @menu.menu_items.each do |item|
       @info << Recipe.find(item.recipe_id)
     end
-    @all = {menu: @menu, menu_items: @info}
+    @all = {menu: @menu_hash, menu_items: @info}
     render json: @all
   end
 end
--- madamerouge/app/controllers/recipes_controller.rb
+++ madamerouge/app/controllers/recipes_controller.rb
@@ -6,8 +6,8 @@ class RecipesController < ApplicationController
 
   def show
     @recipe = Recipe.find_by(id: params[:id])
-    @ingredients = @recipe.ingredients
-    @recetta = {recipe: @recipe, ingredients: @ingredients}
+    @ingredients = @recipe.ingredients.order(:item_id)
+    @recetta = {recipe: @recipe, ingredients: @ingredients, user: @recipe.user.username, items: @recipe.items.order(:id) }
     render json: @recetta
   end
 end
