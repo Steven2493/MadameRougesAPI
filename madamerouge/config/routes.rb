4af7d204a8b55ff5a839bf01b4fb7ea460ac39e9
Sofia Forsell <email": null>
2017-08-14 20:16:15 -0400

    add menu, session, user controllers, and routes , but not for recipes

--- madamerouge/config/routes.rb
+++ madamerouge/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "menus#index"
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
+  get "/logout", to: "sessions#destroy"
 end
