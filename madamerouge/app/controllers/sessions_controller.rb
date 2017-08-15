b67b413e8c1e1c8e41fe35939b4ddcb34c43791d
Sofia Forsell <email": null>
2017-08-15 15:38:26 -0400

    added login functionality

--- madamerouge/app/controllers/sessions_controller.rb
+++ madamerouge/app/controllers/sessions_controller.rb
@@ -4,15 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-
     @user = User.find_by(email: params[:user][:email])
 
     if @user && @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
-      redirect_to "/"
+      render json: {id: @user.id}
     else
-     @error = "Something went wrong, make sure your email/password is correct"
-      render 'new'
+      render :status => 400
     end
   end
 
